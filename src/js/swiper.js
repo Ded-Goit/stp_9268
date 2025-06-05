cb6837894c6d0aa37fea8710d2e49f8cfbab3676
Evgenii <zheniakyril@gmail.com>
2025-06-05 19:43:40 +0300

    fix swipers

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,32 +1,46 @@
+
 import Swiper from 'swiper';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-let swiperInstance = null;
-
-function initCandySwiper() {
+function initSwipers() {
   const isMobile = window.innerWidth < 1200;
+  const swiperElements = document.querySelectorAll('.swiper');
 
-  if (isMobile && !swiperInstance) {
-      swiperInstance = new Swiper('.swiper', {
+  swiperElements.forEach((swiperEl) => {
+    const paginationEl = swiperEl.querySelector('.swiper-pagination');
+
+    if (isMobile) {
+      if (!swiperEl.swiperInstance) {
+        swiperEl.swiperInstance = new Swiper(swiperEl, {
+          modules: [Pagination],
           autoHeight: true,
-        loop: true,
-      modules: [Pagination],
-      slidesPerView: 1,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
-  }
-
-  if (!isMobile && swiperInstance) {
-    swiperInstance.destroy(true, true);
-    swiperInstance = null;
-  }
+          loop: true,
+          slidesPerView: 1,
+          spaceBetween: 30,
+          pagination: {
+            el: paginationEl,
+            clickable: true,
+          },
+        });
+      }
+    } else {
+      if (swiperEl.swiperInstance) {
+        swiperEl.swiperInstance.destroy(true, true);
+        swiperEl.swiperInstance = null;
+      }
+    }
+  });
 }
 
-window.addEventListener('load', initCandySwiper);
-window.addEventListener('resize', initCandySwiper);
+function debounce(func, delay) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, delay);
+  };
+}
+
+window.addEventListener('load', initSwipers);
+window.addEventListener('resize', debounce(initSwipers, 200));
